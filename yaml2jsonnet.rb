9e5a470e8c23d3c1611f30a80d8db24fa5441e80
Marko Mikulicic <mmikulicic@gmail.com>
2023-03-29 12:30:05 +0200

    Update forumla version

--- yaml2jsonnet.rb
+++ yaml2jsonnet.rb
@@ -3,7 +3,7 @@ class Yaml2jsonnet < Formula
   homepage "https://github.com/kubecfg/yaml2jsonnet"
   url "https://github.com/kubecfg/yaml2jsonnet/releases/download/v0.2.0/yaml2jsonnet"
   sha256 "f46d33f3bb44745ba09154df32cf3067c13ce59f01f434341a8a9026a36116b8"
-  version "0.1.0"
+  version "0.2.0"
 
   def install
     bin.install "yaml2jsonnet"
